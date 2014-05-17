50a134492ff4642fcd9cda75f485d0c51a65da90
Jaydeep <jaydeep.w@gmail.com>
2014-05-17 11:21:04 +0530

    some changes

--- Utils/src/nl/changer/android/opensource/JSONUtils.java
+++ Utils/src/nl/changer/android/opensource/JSONUtils.java
@@ -1,10 +1,14 @@
 package nl.changer.android.opensource;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
-import java.util.*;
 
 public class JSONUtils {
     public static Object toJSON(Object object) throws JSONException {
